d87cf6a81cd09e878f904ab68412a2aab4afb93b
Kelly Mears <developers@tinypixel.dev>
2019-11-28 22:49:45 -0800

    add demo plugin

---
+++ demo-plugin/block/resources/assets/scripts/components/edit.js
@@ -0,0 +1,33 @@
+// @wordpress
+import { __ } from '@wordpress/i18n'
+import { InnerBlocks, RichText } from '@wordpress/block-editor'
+
+// exports
+const edit = ({
+  className,
+  attributes,
+  setAttributes
+}) => {
+  const {heading} = attributes
+
+  const onChange = {
+    heading: heading => setAttributes({heading})
+  }
+
+  return (
+    <div className={className}>
+      <div className={`${className}__column-a`}>
+        <RichText
+          el={`div`}
+          className={`${className}__column-a-heading`}
+          value={heading && heading}
+          onChange={onChange.heading} />
+      </div>
+      <div className={`${className}__column-b`}>
+        <InnerBlocks />
+      </div>
+    </div>
+  )
+}
+
+export { edit }
